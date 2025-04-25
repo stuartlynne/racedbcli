8a24c5c0bb5a23582527eb1d363e4cd7263bfc77
Stuart Lynne <stuart.lynne@gmail.com>
2025-04-25 10:44:47 -0700

    add bikereg.py

---
+++ bikereg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Convienence wrapper for running uciid directly from source tree.
+
+
+from bikereg.bikereg import main
+
+if __name__ == '__main__':
+    main()
