544406ddd1328fa717da9132176ffe41dc48da22
Stuart Lynne <stuart.lynne@gmail.com>
2025-09-03 18:17:18 -0700

    ccn helper

---
+++ ccn.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Convienence wrapper for running uciid directly from source tree.
+
+
+from ccn.ccn import main
+
+if __name__ == '__main__':
+    main()
